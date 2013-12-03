0d727646b5fbe3a5395510e654ba2d3e828df1ef
Mark Evans <mark@new-bamboo.co.uk>
2013-12-03 10:25:08 +0000

    removed duplicated region

--- lib/dragonfly/s3_data_store.rb
+++ lib/dragonfly/s3_data_store.rb
@@ -17,7 +17,6 @@ module Dragonfly
       'ap-southeast-1' => 's3-ap-southeast-1.amazonaws.com',
       'ap-southeast-2' => 's3-ap-southeast-2.amazonaws.com',
       'eu-west-1' => 's3-eu-west-1.amazonaws.com',
-      'sa-east-1' => 's3-sa-east-1.amazonaws.com',
       'sa-east-1' => 's3-sa-east-1.amazonaws.com'
     }
 
