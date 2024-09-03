cf08ef1148a813d6a3ef17bb0e847cee21c6fa89
Mark Evans <mark.evans@sedna.com>
2024-09-03 13:52:43 +0100

    bumped rspec

--- dragonfly-s3_data_store.gemspec
+++ dragonfly-s3_data_store.gemspec
@@ -20,5 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "dragonfly", "~> 1.0"
   spec.add_runtime_dependency "fog-aws"
-  spec.add_development_dependency "rspec", "~> 2.0"
+  spec.add_development_dependency "rspec", "~> 3.0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.expect_with(:rspec) do |expectations|
+    expectations.syntax = [:should, :expect]
+  end
+  config.mock_with(:rspec) do |mocks|
+    mocks.syntax = [:should, :expect]
+  end
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
